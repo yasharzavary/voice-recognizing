f9f872a5d8263d6ce4cebd76923ef6bca3bf6ba2
yasharzavary <zavaryyashar009@gmail.com>
2023-06-21 09:10:58 +0430

    database build in mysql

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
 # created by: yashar zavary rezaie
+"""
+    voice recognition:
+    system will recognize voice and answer specific answer to the User 
+    it will check words in the sentence of the user and select primary words and get specific answer
+    
+"""
