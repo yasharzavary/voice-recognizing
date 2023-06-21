e3673a603d431a8a667a609031d9f8168f68fdba
yasharzavary <zavaryyashar009@gmail.com>
2023-06-21 09:20:50 +0430

    main root graphic finish

--- main.py
+++ main.py
@@ -5,3 +5,28 @@
     it will check words in the sentence of the user and select primary words and get specific answer
     
 """
+from tkinter import *
+
+# my main root
+mainRoot=Tk()
+# size of the main root 
+w=500
+h=500
+# sizing the window page
+screenW=mainRoot.winfo_screenwidth()
+screenh=mainRoot.winfo_screenheight()
+x=(screenW/2) - (w/2)
+y=(screenh/2) - (h/2)
+
+# set geometry of main root and set resize False
+mainRoot.geometry('%dx%d+%d+%d'%(w, h, x, y))
+mainRoot.resizable(width=False, height=False)
+
+# set title and icon of the product
+mainRoot.title('sadoos')
+mainRoot.iconbitmap('icons\mainRoot.ico')
+
+
+
+mainRoot.mainloop()
+
